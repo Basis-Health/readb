08556380541c8744c2cdeb9214f5ed9f547a149e
Not Alex <alex@totallynotmyemail.com>
2023-08-07 01:38:29 +0200

    added docs

--- src/database.rs
+++ src/database.rs
@@ -5,15 +5,28 @@ use crate::index_table::IndexTable;
 use crate::io::Loader;
 use crate::io::loader::LazyLoader;
 
+/// The main database structure.
+///
+/// Represents the core of the database, managing the index table, cache, and data loading.
+///
+/// # Generic Parameters
+/// - `C`: The type representing the cache mechanism. Must implement the `Cache` trait.
+/// - `L`: The type responsible for data loading. Must implement the `Loader` trait.
 pub struct Database<C: Cache, L: Loader> {
     index_table: Box<dyn IndexTable>,
     cache: C,
     loader: L,
 }
 
+/// Configuration settings required to initialize a [`Database`].
+///
+/// It provides customizable settings like the storage path, cache size, and the type of index.
 pub struct DatabaseSettings {
+    /// Path to the database's directory.
     pub path: Option<PathBuf>,
+    /// Size of the cache.
     pub cache_size: Option<usize>,
+    /// Type of the index table.
     pub index_type: IndexType,
 }
 
@@ -28,6 +41,12 @@ impl Default for DatabaseSettings {
 }
 
 impl<C: Cache> Database<C, LazyLoader> {
+    /// Constructs a new `Database` instance with the specified settings.
+    ///
+    /// # Errors
+    /// Returns an error if:
+    /// - The path is not provided.
+    /// - The provided path isn't a directory (unless the "ignore-path-check" feature is enabled).
     pub fn new(settings: DatabaseSettings) -> anyhow::Result<Self> {
         if settings.path.is_none() {
             return Err(anyhow::anyhow!("Path is required"));
@@ -56,6 +75,10 @@ impl<C: Cache> Database<C, LazyLoader> {
 
     }
 
+    /// Constructs a new `Database` instance with default settings.
+    ///
+    /// # Parameters
+    /// - `location`: Path to the database's directory.
     pub fn new_default(location: PathBuf) -> Self {
         let settings = DatabaseSettings {
             path: Some(location),
@@ -64,6 +87,16 @@ impl<C: Cache> Database<C, LazyLoader> {
         Self::new(settings).unwrap()
     }
 
+    /// Retrieves the value associated with a given key.
+    ///
+    /// This method will first check the index, then the cache, and finally loads from disk if necessary.
+    ///
+    /// # Parameters
+    /// - `key`: The key for which the value should be fetched.
+    ///
+    /// # Returns
+    /// - `Some(String)` if the key is found.
+    /// - `None` if the key doesn't exist or data loading fails.
     pub fn get(&mut self, key: &str) -> anyhow::Result<Option<String>> {
         let index = self.index_table.get(key);
         if index.is_none() {
@@ -86,6 +119,16 @@ impl<C: Cache> Database<C, LazyLoader> {
         Ok(Some(d))
     }
 
+    /// Associates an existing key with a new key.
+    ///
+    /// This effectively creates an alias for the old key.
+    ///
+    /// # Parameters
+    /// - `old`: The existing key.
+    /// - `new`: The new alias for the key.
+    ///
+    /// # Errors
+    /// Returns an error if the old key is not found in the index table.
     pub fn link(&mut self, old: &str, new: &str) -> anyhow::Result<()> {
         let index = self.index_table.get(old);
         if index.is_none() {
@@ -95,10 +138,16 @@ impl<C: Cache> Database<C, LazyLoader> {
         self.index_table.insert(new.to_string(), *index)
     }
 
+    /// Deletes a key and its associated value from the database.
+    ///
+    /// # Parameters
+    /// - `key`: The key to be removed.
     pub fn delete(&mut self, key: &str) -> anyhow::Result<()> {
         self.index_table.delete(key)
     }
 
+    /// Commits the current state of the database, ensuring data persistence.
+    /// Note, this only commits the index table.
     pub fn persist(&mut self) -> anyhow::Result<()> {
         self.index_table.persist()
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+//! # Readatabase (readb)
+//!
+//! `readb` is a simple, embedded read-only key-value database implemented in Rust. Its emphasis on performance
+//! makes it exceptionally faster than some other databases like `sled` (refer to the benchmark section in the README).
+//!
+//! ## Key Features:
+//! - **Fast and Efficient**: Leverages caching to accelerate frequently accessed reads.
+//! - **Modular Design**: Seamlessly switch between various indexing strategies and caching methods.
+//! - **Lock-Free Reads**: Enables concurrent reads without locks, improving throughput.
+//!
+//! ## Limitations:
+//! - **Read-Only**: Designed for read operations. Writing is not supported.
+//! - **One-Time Indexing**: Data is indexed once at initialization, making it unsuitable for dynamic data additions.
+//!
+//! ## Main Functionality:
+//! ### Base API:
+//! - `new`: Constructs a new database.
+//! - `get`: Retrieves the value associated with a given key.
+//! - `link`: Aliases one key to another.
+//! - `delete`: Removes a key. Links and actual data remain unaffected.
+//! - `persist`: Saves new links to the database.
+//!
+//! ### `index-write` Feature:
+//! When the `index-write` feature is enabled, additional functionalities become available:
+//! - `new_index_table`: Initializes a new index table.
+//!     - `get`: Fetches the value tied to a key.
+//!     - `insert`: Adds a new key-value pair to the index table.
+//!     - `load`: Reads the index table from a specified path.
+//!     - `persist`: Commits the index table to a specified path.
+//!
+//! ### `remote-cloning` Feature:
+//! With the `remote-cloning` feature enabled:
+//! - `clone_from`: Copies the database from a remote address to a local directory.
+//!     - `address`: The remote source.
+//!     - `path`: The local destination.
+//!     - `compression`: Specifies the type of compression to use during transfer. If `None`, no compression is applied.
+//!
+//! For a detailed guide and benchmarking, please refer to the README.
+
 mod cache;
 mod io;
 pub(crate) mod index_table;
